4889d11f14573117133f177ead7eae141be47b0a
soulimane <soulimane.kamni@ensma.fr>
2021-11-30 10:49:45 +0100

    module icon added and description improved

--- __manifest__.py
+++ __manifest__.py
@@ -1,23 +1,23 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "course_management_system",
+    'name': "Course management",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+       Easiest way for course management""",
 
     'description': """
-        Long description of module's purpose
+        This module allows the management of the course centers, schools, etc.
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "SoulSoft",
+    'website': "http://www.soulsoft.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
     # for the full list
     'category': 'Uncategorized',
     'version': '0.1',
+    'application':True,
 
     # any module necessary for this one to work correctly
     'depends': ['base'],
