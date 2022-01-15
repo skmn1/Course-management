9a49e2cd522112255e870a771447b2a32cd0cb28
soulimane <soulimane.kamni@ensma.fr>
2022-01-15 23:10:54 +0100

    groups and access rights created and tested
    two groups were created : assistant and app manager
    the assistant has read only right at the moment

--- __manifest__.py
+++ __manifest__.py
@@ -24,7 +24,8 @@
 
     # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
+        'security/groups.xml',
+        'security/ir.model.access.csv',
         # 'views/views.xml',
         'views/course_view.xml',
         'views/participant_view.xml',
